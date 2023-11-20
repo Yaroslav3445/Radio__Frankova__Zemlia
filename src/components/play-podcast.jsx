081b9982ae3dc1c34d310522bb33ef2ad881df92
Prystash Yaroslav <prystash.yaroslav@gmail.com>
2023-11-20 21:32:49 +0200

    fiftee commit

--- src/components/play-podcast.jsx
+++ src/components/play-podcast.jsx
@@ -1,29 +1,46 @@
-import React, { useState } from 'react';
-import { useLocation, useParams, Link, useRef } from 'react-router-dom';
+import React, { useState, useRef, useEffect } from 'react';
+import { useLocation, useParams, Link } from 'react-router-dom';
 import PlayPodcastStyles from '../style/play-podcast.module.scss';
 import podcastData from '../data/podcast.json';
-import '../style/reset.scss'
-const playPodcast = () => {
+import '../style/reset.scss';
+
+const PlayPodcast = () => {
     const { id } = useParams();
     const location = useLocation();
-    const [volume, setVolume] = useState(1)
+    const [volume, setVolume] = useState(100);
+    const [pause, setPause] = useState(true)
     const audioRef = useRef(null);
     const { podcastData: currentPodcastData } = location.state || {};
 
-    const currentPodcastIndex = podcastData.findIndex(podcast => podcast.id === parseInt(id, 10));
+    const currentPodcastIndex = podcastData.findIndex((podcast) => podcast.id === parseInt(id, 10));
     const nextPodcastIndex = (currentPodcastIndex + 1) % podcastData.length;
     const prevPodcastIndex = (currentPodcastIndex - 1 + podcastData.length) % podcastData.length;
 
     const nextPodcastId = podcastData[nextPodcastIndex].id;
     const prevPodcastId = podcastData[prevPodcastIndex].id;
+
     const volumeMove = (event) => {
-        const newVolume = parseFloat(event.target.value)
-        setVolume(newVolume)
-        console.log(volume)
+        const newVolume = parseFloat(event.target.value);
+        setVolume(newVolume);
+
         if (audioRef.current) {
-            audioRef.current.volume = newVolume / 100; // Гучність повинна бути в діапазоні від 0 до 1
+            audioRef.current.volume = newVolume / 100;
         }
     };
+    const handlePause = () => {
+        setPause(!pause)
+        if (pause) {
+            audioRef.current.pause()
+        } else {
+            audioRef.current.play()
+        }
+    }
+    useEffect(() => {
+        if (audioRef.current) {
+            audioRef.current.volume = volume / 100;
+        }
+    }, [volume]);
+
     return (
         <>
             <section>
@@ -37,33 +54,46 @@ const playPodcast = () => {
                         <div className={PlayPodcastStyles.listen__player}>
                             <Link to={`/play-podcast/${prevPodcastId}`} state={{ podcastData: podcastData[prevPodcastIndex] }}>
                                 <button className={PlayPodcastStyles.listen__goPodcast} type="button">
-                                    <div className={`${PlayPodcastStyles['listen__triagle']} ${PlayPodcastStyles['listen__triagle-2']}   ${PlayPodcastStyles['listen__triagleLeft']}`}></div>
-                                    <div className={`${PlayPodcastStyles['listen__triagle']} ${PlayPodcastStyles['listen__triagle-2']}`}></div>
+                                    <div
+                                        className={`${PlayPodcastStyles['listen__triagle']} ${PlayPodcastStyles['listen__triagle-2']}   ${PlayPodcastStyles['listen__triagleLeft']}`}
+                                    ></div>
+                                    <div
+                                        className={`${PlayPodcastStyles['listen__triagle']} ${PlayPodcastStyles['listen__triagle-2']}`}
+                                    ></div>
                                 </button>
                             </Link>
-                            <audio controls src={currentPodcastData.audioUrl} volume={volume} className={`${PlayPodcastStyles['listen__audio']}`} controlsList="nodownload nofullscreen"></audio>
-                            <input
-                                type="range"
-                                min={0}
-                                max={100}
-                                step={1}
-                                value={volume}
-                                onChange={volumeMove}
-                                className="custom-range-slider"
-                            />
+                            <audio
+                                ref={audioRef}
+                                src={currentPodcastData.audioUrl}
+                                volume={volume / 100}
+                                className={`${PlayPodcastStyles['listen__audio']}`}
+                                controlsList="nodownload nofullscreen"
+                            ></audio>
+                            <button className={PlayPodcastStyles.listen__play} onClick={handlePause} type="button"><div  className={PlayPodcastStyles.listen__mainTriagle}>
+                                </div></button>
                             <Link to={`/play-podcast/${nextPodcastId}`} state={{ podcastData: podcastData[nextPodcastIndex] }}>
                                 <button className={PlayPodcastStyles.listen__goPodcast} type="button">
                                     <div className={`${PlayPodcastStyles['listen__triagle']}`}></div>
-                                    <div className={`${PlayPodcastStyles['listen__triagle']}  ${PlayPodcastStyles['listen__triagleRight']}`}></div>
+                                    <div
+                                        className={`${PlayPodcastStyles['listen__triagle']}  ${PlayPodcastStyles['listen__triagleRight']}`}
+                                    ></div>
                                 </button>
                             </Link>
                         </div>
+                        <input
+                            type="range"
+                            min={0} 
+                            max={100}
+                            step={1}
+                            value={volume}
+                            onChange={volumeMove}
+                            className={PlayPodcastStyles.listen__sound}
+                        />
                     </div>
                 </div>
-
-            </section >
+            </section>
         </>
     );
 };
 
-export default playPodcast;
+export default PlayPodcast;
